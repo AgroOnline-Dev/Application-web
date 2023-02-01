033d4b530f13cc55df875be5c971c46f44fa9ffc
noumbissi16 <noumbissistael@gmail.com>
2023-02-01 08:40:21 +0100

    update number 2 of app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Updated....
+          Edit <code>src/App.js</code> Update number 2....
         </p>
         <a
           className="App-link"
