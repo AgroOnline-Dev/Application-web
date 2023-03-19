99a9697d603dfcfbee769f682d94f6df1fbffcf1
noumbissi16 <noumbissistael@gmail.com>
2023-03-19 10:09:53 +0100

    finished with navbar and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,50 +9,52 @@ function Footer() {
   return (
     <>
       <footer className="footer">
-        <div className="container">
-          <div className="row">
-            <div className="footer-col">
-              <Link to="/">
-                <img className="logo" src={logo} alt="logo" />
+        <div className="footer-col">
+          <Link to="/">
+            <img className="logo" src={logo} alt="logo" />
+          </Link>
+        </div>
+        <div className="footer-col">
+          <h4>Services</h4>
+          <ul className="footer-links">
+            <li>
+              <Link className="footer-link" to="/ecommerce">
+                E-commerce
               </Link>
-            </div>
-            <div className="footer-col">
-              <h4>Services</h4>
-              <ul>
-                <li>
-                  <Link to="/ecommerce">E-commerce</Link>
-                </li>
-                <li>
-                  <Link to="/accomp-tech">AccompTech</Link>
-                </li>
-                <li>
-                  <Link to="/investissement">Investissement</Link>
-                </li>
-              </ul>
-            </div>
-            <div className="footer-col">
-              <h4>Suivez nous</h4>
-              <div>
-                <Link to="/">
-                  <img className="social-links" src={facebook} />
-                </Link>
-                <Link to="/">
-                  <img className="social-links" src={whatsapp} />
-                </Link>
-                <Link to="/">
-                  <img className="social-links" src={twitter} />
-                </Link>
-              </div>
-            </div>
-            <div className="footer-col">
-              <h4>Contact</h4>
-              <ul>
-                <li> +237 674 458 258</li>
-                <li> +237 674 458 258</li>
-                <li> agroonloneo@gmail.com</li>
-              </ul>
-            </div>
+            </li>
+            <li>
+              <Link className="footer-link" to="/accomp-tech">
+                AccompTech
+              </Link>
+            </li>
+            <li>
+              <Link className="footer-link" to="/investissement">
+                Investissement
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-col">
+          <h4>Suivez nous</h4>
+          <div>
+            <Link to="/">
+              <img className="social-links" id="facebook" src={facebook} />
+            </Link>
+            <Link to="/">
+              <img className="social-links" src={whatsapp} />
+            </Link>
+            <Link to="/">
+              <img className="social-links" src={twitter} />
+            </Link>
           </div>
+        </div>
+        <div className="footer-col">
+          <h4>Contact</h4>
+          <ul className="footer-contact">
+            <li> +237 674 458 258</li>
+            <li> +237 674 458 258</li>
+            <li> agroonloneo@gmail.com</li>
+          </ul>
         </div>
       </footer>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/Logo.png";
+import search from "../assets/Search Icon.png";
+import cart from "../assets/Path 6.png";
+import profileOffline from "../assets/profile.png";
 
 function Navbar() {
   return (
-    <nav>
-      <img src={logo} alt="logo" />
-      <ul>
-        <li>
-          <Link className="nav-link" to="/">
-            Accueil
+    <>
+      <nav>
+        <div className="nav-logo">
+          <Link to="/">
+            <img src={logo} alt="logo" />
           </Link>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <Link className="nav-link" to="/ecommerce">
-            E-commerce
-          </Link>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <Link className="nav-link" to="/accomp-tech">
-            AccompTech
-          </Link>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <Link className="nav-link" to="/investissement">
-            Investissement
-          </Link>
-        </li>
-      </ul>
-    </nav>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <Link className="nav-link" to="/">
+              Accueil
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link" to="/ecommerce">
+              E-commerce
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link" to="/accomp-tech">
+              AccompTech
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link" to="/investissement">
+              Investissement
+            </Link>
+          </li>
+        </ul>
+        <div>
+          <img className="nav-icons" src={search} alt="search" />
+          <img className="nav-icons" src={cart} alt="shopping cart" />
+          <img
+            className="nav-icons profile"
+            src={profileOffline}
+            alt="profile"
+          />
+        </div>
+      </nav>
+    </>
   );
 }
 
